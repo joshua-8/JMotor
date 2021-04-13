10d331711a274c1ffed4f67ad61f3eb8bda8a151
joshua-8 <joshua-8@users.noreply.github.com>
2021-04-13 05:09:31 +0000

    deploy: c0a1e54a439a16cb0537e30f7339369312450f93

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['jmotordriver_17',['JMotorDriver',['../class_j_motor_driver.html',1,'']]],
-  ['jmotordriveravrl293_18',['JMotorDriverAvrL293',['../class_j_motor_driver_avr_l293.html',1,'']]],
-  ['jmotordriveravrpwm_19',['JMotorDriverAvrPWM',['../class_j_motor_driver_avr_p_w_m.html',1,'']]],
-  ['jmotordriveravrpwmdir_20',['JMotorDriverAvrPWMDir',['../class_j_motor_driver_avr_p_w_m_dir.html',1,'']]],
-  ['jmotordriveravrservo_21',['JMotorDriverAvrServo',['../class_j_motor_driver_avr_servo.html',1,'']]],
-  ['jmotordriveresp32l293d_22',['JMotorDriverESP32L293d',['../class_j_motor_driver_e_s_p32_l293d.html',1,'']]],
-  ['jmotordriveresp32pwm_23',['JMotorDriverEsp32PWM',['../class_j_motor_driver_esp32_p_w_m.html',1,'']]],
-  ['jmotordriveresp32servo_24',['JMotorDriverEsp32Servo',['../class_j_motor_driver_esp32_servo.html',1,'']]]
+  ['jmotordriver_18',['JMotorDriver',['../class_j_motor_driver.html',1,'']]],
+  ['jmotordriveravrl293_19',['JMotorDriverAvrL293',['../class_j_motor_driver_avr_l293.html',1,'']]],
+  ['jmotordriveravrpwm_20',['JMotorDriverAvrPWM',['../class_j_motor_driver_avr_p_w_m.html',1,'']]],
+  ['jmotordriveravrpwmdir_21',['JMotorDriverAvrPWMDir',['../class_j_motor_driver_avr_p_w_m_dir.html',1,'']]],
+  ['jmotordriveravrservo_22',['JMotorDriverAvrServo',['../class_j_motor_driver_avr_servo.html',1,'']]],
+  ['jmotordriveresp32l293_23',['JMotorDriverESP32L293',['../class_j_motor_driver_e_s_p32_l293.html',1,'']]],
+  ['jmotordriveresp32l293d_24',['JMotorDriverESP32L293d',['../class_j_motor_driver_e_s_p32_l293d.html',1,'']]],
+  ['jmotordriveresp32pwm_25',['JMotorDriverEsp32PWM',['../class_j_motor_driver_esp32_p_w_m.html',1,'']]],
+  ['jmotordriveresp32servo_26',['JMotorDriverEsp32Servo',['../class_j_motor_driver_esp32_servo.html',1,'']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,11 @@
 var searchData=
 [
-  ['jmotordriveravrl293_29',['JMotorDriverAvrL293',['../class_j_motor_driver_avr_l293.html#a060fee9bf01df5401cadfa910d8dbdf0',1,'JMotorDriverAvrL293']]],
-  ['jmotordriveravrpwmdir_30',['JMotorDriverAvrPWMDir',['../class_j_motor_driver_avr_p_w_m_dir.html#a1eeb5fb9f981cb92afe6024da67d7407',1,'JMotorDriverAvrPWMDir']]]
+  ['jmotordriveravrl293_31',['JMotorDriverAvrL293',['../class_j_motor_driver_avr_l293.html#a060fee9bf01df5401cadfa910d8dbdf0',1,'JMotorDriverAvrL293']]],
+  ['jmotordriveravrpwm_32',['JMotorDriverAvrPWM',['../class_j_motor_driver_avr_p_w_m.html#ad47046e668c419621b1ee36ec3d4e8cc',1,'JMotorDriverAvrPWM']]],
+  ['jmotordriveravrpwmdir_33',['JMotorDriverAvrPWMDir',['../class_j_motor_driver_avr_p_w_m_dir.html#a1eeb5fb9f981cb92afe6024da67d7407',1,'JMotorDriverAvrPWMDir']]],
+  ['jmotordriveravrservo_34',['JMotorDriverAvrServo',['../class_j_motor_driver_avr_servo.html#a0de86a3ed003fba2e1948b1f2944416c',1,'JMotorDriverAvrServo']]],
+  ['jmotordriveresp32l293_35',['JMotorDriverESP32L293',['../class_j_motor_driver_e_s_p32_l293.html#a4e593e3d0a53edba9856bc993a116326',1,'JMotorDriverESP32L293::JMotorDriverESP32L293(int _ch, int _enablePin, int _i1, int _i2)'],['../class_j_motor_driver_e_s_p32_l293.html#a532faf943a99dd12c0a1e453a53f8d9b',1,'JMotorDriverESP32L293::JMotorDriverESP32L293(int _ch, int _enPin, int _i1, int _i2, int freq, int resolution)']]],
+  ['jmotordriveresp32l293d_36',['JMotorDriverESP32L293d',['../class_j_motor_driver_e_s_p32_l293d.html#a5b427aade397bbde6b35d4c7246733a5',1,'JMotorDriverESP32L293d::JMotorDriverESP32L293d(int _ch, int _enablePin, int _dirPin, boolean _rev=false)'],['../class_j_motor_driver_e_s_p32_l293d.html#aa4ecc9b56c20651b7ce62f46ec9b81d8',1,'JMotorDriverESP32L293d::JMotorDriverESP32L293d(int _ch, int _enPin, int _dirPin, int freq, int resolution, boolean _rev=false)']]],
+  ['jmotordriveresp32pwm_37',['JMotorDriverEsp32PWM',['../class_j_motor_driver_esp32_p_w_m.html#ab74365a91e34c1e7d31f8b6c1b153db5',1,'JMotorDriverEsp32PWM::JMotorDriverEsp32PWM(int _ch, int _pin, boolean _disableState=LOW)'],['../class_j_motor_driver_esp32_p_w_m.html#a6c64fe375f5f69329a9114e7b16a8ef9',1,'JMotorDriverEsp32PWM::JMotorDriverEsp32PWM(int _ch, int _pin, int freq, int resolution, boolean _disableState=LOW)']]],
+  ['jmotordriveresp32servo_38',['JMotorDriverEsp32Servo',['../class_j_motor_driver_esp32_servo.html#add45cbcf20e45d1a5442e192701a04f9',1,'JMotorDriverEsp32Servo']]]
 ];
