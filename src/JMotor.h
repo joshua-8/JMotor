f41d37fe614f9727a347e135ee7f9df663668944
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2021-04-12 19:07:10 -0700

    adding comments

--- src/JMotor.h
+++ src/JMotor.h
@@ -1,6 +1,8 @@
 /*! \mainpage
- * JMotor is a library for controlling motors
- * 
+ * JMotor library
+ * github repository: https://github.com/joshua-8/JMotor
+ * reference docs: https://joshua-8.github.io/JMotor/md_readme.html
+ * tested on the following boards:   ESP32, Arduino AVR (specifically nano)
  */
 
 #ifndef J_MOTOR_LIB_H
