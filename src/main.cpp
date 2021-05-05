0ed84fb953b21f67205277c5e232b265357543de
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2021-05-05 12:13:24 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,9 @@
 #define port5 4, port5Pin
 //           PWM_CH, EN_PIN, PIN1, PIN2
 #define portA 3, 23, 22, 19
-#define portB 1, 5, 18, 21
+#define portB 1, 5, 21, 18
 #define portC 2, 17, 16, 12
-#define portD 0, 13, 4, 14
+#define portD 0, 13, 14, 4
 
 #define inport1 39
 #define inport2 34
@@ -23,46 +23,33 @@
 
 #define batMonitorPin 36
 
-#define dacUnitsPerVolt 413
+#define dacUnitsPerVolt 380
 
-JMotorDriverEsp32L293 myDriver = JMotorDriverEsp32L293(portA);
+JMotorDriverEsp32L293 myDriverD = JMotorDriverEsp32L293(portD);
 
-JVoltageCompMeasure<20> voltageComp = JVoltageCompMeasure<20>(batMonitorPin, dacUnitsPerVolt, 1.0);
-JMotorCompStandardConfig ttConfig = JMotorCompStandardConfig(1.4, .652, 2.6, 1.3, 4, 1.6, 100);
+JEncoderPWMAbsoluteAttachInterrupt encoder = JEncoderPWMAbsoluteAttachInterrupt(port5Pin, JEncoderPWMAbsolute_AS5048settings, true, 1, 50000, 1000);
+jENCODER_MAKE_ISR_MACRO(encoder);
+
+JVoltageCompMeasure<20> voltageComp = JVoltageCompMeasure<20>(batMonitorPin, dacUnitsPerVolt);
+JMotorCompStandardConfig ttConfig = JMotorCompStandardConfig(1.9, .553, 3.2, 1.24, 4.6, 1.89, 100);
 JMotorCompStandard myMotorCompensator = JMotorCompStandard(voltageComp, ttConfig, 1.0);
-JMotorControllerBasicOpen myController = JMotorControllerBasicOpen(myDriver, myMotorCompensator, .5);
-// JEncoderPWMAbsoluteAttachInterrupt encoder = JEncoderPWMAbsoluteAttachInterrupt(inport1, JEncoderPWMAbsolute_AS5048settings, true, 1, 50000, 1000);
-// jENCODER_MAKE_ISR_MACRO(encoder);
+// JMotorControllerBasicOpen myController = JMotorControllerBasicOpen(myDriverD, myMotorCompensator);
 
 float vel = 0;
 bool inc = true;
 void setup()
 {
     Serial.begin(9600);
-    myController.enable();
-    // encoder.setUpInterrupts(encoder_jENCODER_ISR);
+    encoder.setUpInterrupts(encoder_jENCODER_ISR);
 }
 void loop()
 {
-    // if (Serial.available()) {
-    //     vel = Serial.parseFloat();
-    //     while (Serial.available()) {
-    //         Serial.read();
-    //     }
-    // }
-    inc ? vel += .01 : vel -= .01;
-    if (abs(vel) > 1)
-        inc = !inc;
-    myDriver.set(vel);
-    //    myController.setVelTarget(vel);
-    // encoder.run();
-    // Serial.print(vel);
-    // Serial.print(",");
-    // Serial.print(myController.getVel());
-    // Serial.print(",");
-    // Serial.print(myController.getDriverSetVal());
-    // Serial.print(",");
-    // Serial.print(encoder.getVel());
-    Serial.println();
+    if (Serial.available()) {
+        vel = Serial.parseFloat();
+        while (Serial.available()) {
+            Serial.read();
+        }
+    }
+
     delay(15);
 }
