acafa02b20cdc812c764b917eb6c929f2754da62
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2021-05-10 09:32:30 -0700

    Update JMotorControllerClosed.h

--- src/JMotorController/JMotorControllerClosed.h
+++ src/JMotorController/JMotorControllerClosed.h
@@ -1,2 +1,35 @@
-// fully implement JMotorController
-// provide access to encoder
+// #ifndef J_MOTOR_CONTROLLER_CLOSED_H
+// #define J_MOTOR_CONTROLLER_CLOSED_H
+// #include "JEncoder/JEncoder.h"
+// #include "JMotorController.h"
+// #include "JMotorControllerBasicOpen.h"
+// #include <Arduino.h>
+// class JMotorControllerClosed : public virtual JMotorController, public JMotorControllerBasicOpen {
+// protected:
+//     float velocity;
+//     float setVal;
+//     float velocityTarget;
+
+// public:
+//     JEncoder& encoder;
+//     JMotorControllerClosed()
+//     {
+//     }
+
+//     void run()
+//     {
+//     }
+//     bool setPosTarget(float _posTarget)
+//     {
+//     }
+//     float getPosTarget()
+//     {
+//     }
+//     float getPos()
+//     {
+//     }
+//     float resetPos(float pos = 0)
+//     {
+//     }
+// };
+// #endif
