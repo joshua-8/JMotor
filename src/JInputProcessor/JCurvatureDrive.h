ade962f4da69acdf6b85b014ff72e3b5797a868d
joshua-8 <joshua_phelps@brown.edu>
2022-06-19 22:38:43 -0400

    fix for curvature drive

--- src/JInputProcessor/JCurvatureDrive.h
+++ src/JInputProcessor/JCurvatureDrive.h
@@ -16,9 +16,11 @@ namespace JCurvatureDrive {
  * @param  reverseTurning: if true, positive turn is clockwise, if false, positive turn is counter clockwise
  * @retval output JTwoDTransform
  */
-inline JTwoDTransform calculate(bool curvatureEnabled, JTwoDTransform input, float minRadius, bool reverseTurning)
+inline JTwoDTransform calculate(bool curvatureEnabled, JTwoDTransform input, float minRadius, bool reverseTurning = false)
 {
     JTwoDTransform output = input;
+    if (reverseTurning)
+        output.theta = -output.theta;
     if (curvatureEnabled) {
         float speed = sqrt(sq(input.x) + sq(input.y));
         output.theta = output.theta * speed / minRadius;
