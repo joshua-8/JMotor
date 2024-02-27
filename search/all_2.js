2ed1a3f4ac89ed122728192e9d96630dd005227a
joshua-8 <joshua-8@users.noreply.github.com>
2024-02-27 02:01:48 +0000

    deploy: 6efd1b95590f7594f56bd7f2fe5bc9ae34c704db

--- search/all_2.js
+++ search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['calc_0',['calc',['../class_j_control_loop_basic.html#af866c812ae16b24d3f8c787bff146667',1,'JControlLoopBasic::calc()'],['../class_j_control_loop.html#a8ab934365e56d62d2ebb56dcecb28f8d',1,'JControlLoop::calc()']]],
+  ['calc_0',['calc',['../class_j_control_loop.html#a8ab934365e56d62d2ebb56dcecb28f8d',1,'JControlLoop::calc()'],['../class_j_control_loop_basic.html#af866c812ae16b24d3f8c787bff146667',1,'JControlLoopBasic::calc()']]],
   ['calcclosestangle_1',['calcClosestAngle',['../class_j_drivetrain_swerve.html#a58b37d90604e34b60275044170c23c93',1,'JDrivetrainSwerve']]],
   ['calculate_2',['calculate',['../class_j_swerve_angle_control_loop.html#a782ba3c8e2c5893b61c7ee5a0ff6d8f2',1,'JSwerveAngleControlLoop::calculate()'],['../class_j_swerve_angle_control_loop_p.html#a6b442061205afdc416cf2da22f99031b',1,'JSwerveAngleControlLoopP::calculate()'],['../namespace_j_deadzone_remover.html#a5444e5cd270d86a85d1367e578599324',1,'JDeadzoneRemover::calculate(JTwoDTransform value, JTwoDTransform start, JTwoDTransform max, JTwoDTransform deadzone={ 0, 0, 0 })'],['../namespace_j_deadzone_remover.html#a2bfa0983c968b342fd4a4fd9b4731876',1,'JDeadzoneRemover::calculate(float value, float start, float max, float deadzone=0)'],['../namespace_j_curvature_drive.html#a4435b4b1bce5ffb49f2c5cdf03b74bb6',1,'JCurvatureDrive::calculate()']]],
   ['centerf_3',['centerF',['../class_j_drivetrain_swerve.html#a3dfbd81b58cc0ca2e2568011975c5bf6',1,'JDrivetrainSwerve']]],
@@ -9,12 +9,13 @@ var searchData=
   ['changesupplyvoltagesetting_6',['changeSupplyVoltageSetting',['../class_j_voltage_comp_const.html#a5200fd8e1e894a45ec817944cbb8f8e4',1,'JVoltageCompConst']]],
   ['changesupplyvoltagevariable_7',['changeSupplyVoltageVariable',['../class_j_voltage_comp_adjust.html#aca1ee78f945cbf6ea7929a4e2715d4eb',1,'JVoltageCompAdjust']]],
   ['channel_8',['channel',['../class_j_motor_driver_t_m_c7300.html#a6d9085fe1b959d83e338deeeeda59856',1,'JMotorDriverTMC7300']]],
-  ['compatible_20breaking_20changes_20between_20some_20versions_9',['there will be backwards compatible breaking changes between some versions',['../md_readme.html#autotoc_md2',1,'']]],
-  ['compensate_10',['compensate',['../class_j_motor_comp_direct.html#ad817d16fe4afb4df25363d9c0f26eb52',1,'JMotorCompDirect::compensate()'],['../class_j_motor_compensator.html#a40a9757a08400f070da51d75ba7d7df8',1,'JMotorCompensator::compensate()'],['../class_j_motor_comp_standard.html#a4efa9461ed6585844857a20fd9530580',1,'JMotorCompStandard::compensate()'],['../class_j_motor_comp_basic.html#ab877f2634927d71378166bedf83728a7',1,'JMotorCompBasic::compensate()']]],
-  ['compensator_11',['compensator',['../class_j_motor_controller_basic.html#a4c1f4b702b721dd056c7dc5e01d3ee03',1,'JMotorControllerBasic::compensator'],['../class_j_motor_controller_closed.html#abb6636240bf21f5663581dcb2b61b838',1,'JMotorControllerClosed::compensator']]],
-  ['conf_5freg_12',['CONF_REG',['../class_j_encoder_a_s5600_i2_c.html#a8e4e8e1fefeb53a9dcb6de83ac9d36f8',1,'JEncoderAS5600I2C']]],
-  ['constrainrange_13',['constrainRange',['../class_j_motor_driver_servo.html#a6e3bab89a79df5d9b7b53700bfc8f71e',1,'JMotorDriverServo']]],
-  ['controlled_14',['controlled',['../class_j_drivetrain_controller_basic.html#ad3255d370caf209b56c500336803856b',1,'JDrivetrainControllerBasic']]],
-  ['controlloop_15',['controlLoop',['../class_j_motor_controller_closed.html#a9791f4a9c6b33ece354bd1ca13f9c87d',1,'JMotorControllerClosed']]],
-  ['ctrlloopout_16',['ctrlLoopOut',['../class_j_control_loop.html#a16dc27b3a6a83fc5ab2fe80aa690ef95',1,'JControlLoop']]]
+  ['cmakelists_2etxt_9',['CMakeLists.txt',['../_c_make_lists_8txt.html',1,'']]],
+  ['compatible_20breaking_20changes_20between_20some_20versions_10',['there will be backwards compatible breaking changes between some versions',['../md_readme.html#autotoc_md2',1,'']]],
+  ['compensate_11',['compensate',['../class_j_motor_comp_basic.html#ab877f2634927d71378166bedf83728a7',1,'JMotorCompBasic::compensate()'],['../class_j_motor_comp_direct.html#ad817d16fe4afb4df25363d9c0f26eb52',1,'JMotorCompDirect::compensate()'],['../class_j_motor_compensator.html#a40a9757a08400f070da51d75ba7d7df8',1,'JMotorCompensator::compensate()'],['../class_j_motor_comp_standard.html#a4efa9461ed6585844857a20fd9530580',1,'JMotorCompStandard::compensate()']]],
+  ['compensator_12',['compensator',['../class_j_motor_controller_basic.html#a4c1f4b702b721dd056c7dc5e01d3ee03',1,'JMotorControllerBasic::compensator'],['../class_j_motor_controller_closed.html#abb6636240bf21f5663581dcb2b61b838',1,'JMotorControllerClosed::compensator']]],
+  ['conf_5freg_13',['CONF_REG',['../class_j_encoder_a_s5600_i2_c.html#a8e4e8e1fefeb53a9dcb6de83ac9d36f8',1,'JEncoderAS5600I2C']]],
+  ['constrainrange_14',['constrainRange',['../class_j_motor_driver_servo.html#a6e3bab89a79df5d9b7b53700bfc8f71e',1,'JMotorDriverServo']]],
+  ['controlled_15',['controlled',['../class_j_drivetrain_controller_basic.html#ad3255d370caf209b56c500336803856b',1,'JDrivetrainControllerBasic']]],
+  ['controlloop_16',['controlLoop',['../class_j_motor_controller_closed.html#a9791f4a9c6b33ece354bd1ca13f9c87d',1,'JMotorControllerClosed']]],
+  ['ctrlloopout_17',['ctrlLoopOut',['../class_j_control_loop.html#a16dc27b3a6a83fc5ab2fe80aa690ef95',1,'JControlLoop']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -3,7 +3,7 @@ var indexSectionsWithContent =
   0: "abcdefghijklmnoprstuvwxyz",
   1: "jmp",
   2: "j",
-  3: "jr",
+  3: "cjr",
   4: "acdefghijmoprstuwz",
   5: "abcdefgijklmnoprstuvwxyz",
   6: "jl"
