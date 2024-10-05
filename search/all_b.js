7bfcf3218873f21cab1d550cc63b5ed4f0948849
joshua-8 <joshua-8@users.noreply.github.com>
2024-10-05 00:32:59 +0000

    deploy: 50e31aa8c1af6f29f60f3c131ef5334494993c1a

--- search/all_b.js
+++ search/all_b.js
@@ -1,17 +1,18 @@
 var searchData=
 [
   ['lastcalcmillis_0',['lastCalcMillis',['../class_j_drivetrain_controller_basic.html#ab881349b991f205f91728c32731f36fc',1,'JDrivetrainControllerBasic']]],
-  ['lastdrivetraindist_1',['lastDrivetrainDist',['../class_j_drivetrain_field_oriented.html#ad891f768d7b8a45e708e9ea2d2cf8dd0',1,'JDrivetrainFieldOriented']]],
-  ['lastimuheading_2',['lastImuHeading',['../class_j_drivetrain_field_oriented.html#a8e0230f206a140ecfa0b99c8dec54495',1,'JDrivetrainFieldOriented']]],
-  ['lastmeasurementmillis_3',['lastMeasurementMillis',['../class_j_servo_current_sensor.html#a6c90157fa33890f18c85284f8da75864',1,'JServoCurrentSensor']]],
-  ['lastmovedmillis_4',['lastmovedmillis',['../class_j_control_loop_basic.html#adbc723bc1b730de4069ef451e650ccc8',1,'JControlLoopBasic::lastMovedMillis'],['../class_j_servo_controller.html#ab8dc06266f3e731961348897d888134b',1,'JServoController::lastMovedMillis']]],
-  ['lastposdeltawrite_5',['lastPosDeltaWrite',['../class_j_motor_controller_open.html#aec0148d6fe541c9e290bc58dc60f6fe6',1,'JMotorControllerOpen']]],
-  ['lastpossetpoint_6',['lastPosSetpoint',['../class_j_motor_controller_closed.html#ac0f9e1fc68c2bee5b09c805dd125ee5b',1,'JMotorControllerClosed']]],
-  ['lastrunmicros_7',['lastrunmicros',['../class_j_motor_controller_basic.html#a175be0e9bf6befcedfe864696db78130',1,'JMotorControllerBasic::lastRunMicros'],['../class_j_motor_controller_closed.html#a3211ccb69573ece34bdbf81de9bcaa1c',1,'JMotorControllerClosed::lastRunMicros']]],
-  ['lastrunopenmicros_8',['lastRunOpenMicros',['../class_j_motor_controller_open.html#aa0c7855bdb18b7a6549f078694a32ea1',1,'JMotorControllerOpen']]],
-  ['left_9',['left',['../class_j_drivetrain_two_side.html#aba6bf82410b8739d4f18856389dfa911',1,'JDrivetrainTwoSide']]],
-  ['leftdist_10',['leftDist',['../class_j_drivetrain_two_side.html#a12887b6921d902ac2e06ba32e92a7834',1,'JDrivetrainTwoSide']]],
-  ['leftvel_11',['leftVel',['../class_j_drivetrain_two_side.html#a7645937e948a739a0784f70a22eadd8c',1,'JDrivetrainTwoSide']]],
-  ['library_12',['JMotor Library',['../md_readme.html',1,'']]],
-  ['limitsetpointdistfromcurrent_13',['limitSetpointDistFromCurrent',['../class_j_motor_controller_closed.html#afbd7ffede859faf8c514efcae0c2096e',1,'JMotorControllerClosed']]]
+  ['lastcheckedic_1',['lastCheckedIC',['../class_j_motor_driver_t_m_c7300.html#a3bd35692d26ad39abed904bc306da69c',1,'JMotorDriverTMC7300']]],
+  ['lastdrivetraindist_2',['lastDrivetrainDist',['../class_j_drivetrain_field_oriented.html#ad891f768d7b8a45e708e9ea2d2cf8dd0',1,'JDrivetrainFieldOriented']]],
+  ['lastimuheading_3',['lastImuHeading',['../class_j_drivetrain_field_oriented.html#a8e0230f206a140ecfa0b99c8dec54495',1,'JDrivetrainFieldOriented']]],
+  ['lastmeasurementmillis_4',['lastMeasurementMillis',['../class_j_servo_current_sensor.html#a6c90157fa33890f18c85284f8da75864',1,'JServoCurrentSensor']]],
+  ['lastmovedmillis_5',['lastmovedmillis',['../class_j_control_loop_basic.html#adbc723bc1b730de4069ef451e650ccc8',1,'JControlLoopBasic::lastMovedMillis'],['../class_j_servo_controller.html#ab8dc06266f3e731961348897d888134b',1,'JServoController::lastMovedMillis']]],
+  ['lastposdeltawrite_6',['lastPosDeltaWrite',['../class_j_motor_controller_open.html#aec0148d6fe541c9e290bc58dc60f6fe6',1,'JMotorControllerOpen']]],
+  ['lastpossetpoint_7',['lastPosSetpoint',['../class_j_motor_controller_closed.html#ac0f9e1fc68c2bee5b09c805dd125ee5b',1,'JMotorControllerClosed']]],
+  ['lastrunmicros_8',['lastrunmicros',['../class_j_motor_controller_basic.html#a175be0e9bf6befcedfe864696db78130',1,'JMotorControllerBasic::lastRunMicros'],['../class_j_motor_controller_closed.html#a3211ccb69573ece34bdbf81de9bcaa1c',1,'JMotorControllerClosed::lastRunMicros']]],
+  ['lastrunopenmicros_9',['lastRunOpenMicros',['../class_j_motor_controller_open.html#aa0c7855bdb18b7a6549f078694a32ea1',1,'JMotorControllerOpen']]],
+  ['left_10',['left',['../class_j_drivetrain_two_side.html#aba6bf82410b8739d4f18856389dfa911',1,'JDrivetrainTwoSide']]],
+  ['leftdist_11',['leftDist',['../class_j_drivetrain_two_side.html#a12887b6921d902ac2e06ba32e92a7834',1,'JDrivetrainTwoSide']]],
+  ['leftvel_12',['leftVel',['../class_j_drivetrain_two_side.html#a7645937e948a739a0784f70a22eadd8c',1,'JDrivetrainTwoSide']]],
+  ['library_13',['JMotor Library',['../md_readme.html',1,'']]],
+  ['limitsetpointdistfromcurrent_14',['limitSetpointDistFromCurrent',['../class_j_motor_controller_closed.html#afbd7ffede859faf8c514efcae0c2096e',1,'JMotorControllerClosed']]]
 ];
