6eba1cc69f44db1ce0e3bcda44c58674eed004c7
joshua-8 <joshua-8@users.noreply.github.com>
2024-10-27 07:33:47 +0000

    deploy: 020deae08d6c1981070138377d5f963879556a62

--- search/variables_4.js
+++ search/variables_4.js
@@ -2,10 +2,11 @@ var searchData=
 [
   ['enabled_0',['enabled',['../class_j_motor_driver_esp32_h_bridge.html#ac33f9b2d8122b057a1e5066e92a2ba67',1,'JMotorDriverEsp32HBridge::enabled'],['../class_j_motor_driver_t_m_c7300.html#a4786e43c5d19ce8b227dde0edeaf6d43',1,'JMotorDriverTMC7300::enabled'],['../class_j_servo_controller.html#afb7350b46d5a35930d546fe0b0bcb60d',1,'JServoController::enabled']]],
   ['enabledmillis_1',['enabledMillis',['../class_j_servo_controller_advanced.html#a9153c59f4460b44fbc3ca95dc7ec5fec',1,'JServoControllerAdvanced']]],
-  ['encoder_2',['encoder',['../class_j_motor_controller_closed.html#a52c5bde3a86b8dbc8aa5936f643a549c',1,'JMotorControllerClosed']]],
-  ['encoderapin_3',['encoderAPin',['../class_j_encoder_quadrature.html#acdfb2bd349c33f5e5119260dba04da3f',1,'JEncoderQuadrature']]],
-  ['encoderbpin_4',['encoderBPin',['../class_j_encoder_quadrature.html#a44ef3124ee19d39ef43b539c4ffe7b11',1,'JEncoderQuadrature']]],
-  ['encoderchannel_5',['encoderChannel',['../class_j_encoder_b_s_e_d.html#a1908855a9d4b8835d69869874d2b187f',1,'JEncoderBSED']]],
-  ['encoderpin_6',['encoderpin',['../class_j_encoder_p_w_m_absolute.html#a7639836ccba8344fed34e24cb3170ec2',1,'JEncoderPWMAbsolute::encoderPin'],['../class_j_encoder_single.html#ab025a271ff5007822fcc083a494bc3a8',1,'JEncoderSingle::encoderPin']]],
-  ['error_7',['error',['../class_j_control_loop.html#a100befc78d818454698a2cccafeb9aa8',1,'JControlLoop']]]
+  ['enablepin_2',['enablePin',['../class_j_motor_driver_t_m_c7300.html#a08717a8b465ff1c991bba93c1ab1b956',1,'JMotorDriverTMC7300']]],
+  ['encoder_3',['encoder',['../class_j_motor_controller_closed.html#a52c5bde3a86b8dbc8aa5936f643a549c',1,'JMotorControllerClosed']]],
+  ['encoderapin_4',['encoderAPin',['../class_j_encoder_quadrature.html#acdfb2bd349c33f5e5119260dba04da3f',1,'JEncoderQuadrature']]],
+  ['encoderbpin_5',['encoderBPin',['../class_j_encoder_quadrature.html#a44ef3124ee19d39ef43b539c4ffe7b11',1,'JEncoderQuadrature']]],
+  ['encoderchannel_6',['encoderChannel',['../class_j_encoder_b_s_e_d.html#a1908855a9d4b8835d69869874d2b187f',1,'JEncoderBSED']]],
+  ['encoderpin_7',['encoderpin',['../class_j_encoder_p_w_m_absolute.html#a7639836ccba8344fed34e24cb3170ec2',1,'JEncoderPWMAbsolute::encoderPin'],['../class_j_encoder_single.html#ab025a271ff5007822fcc083a494bc3a8',1,'JEncoderSingle::encoderPin']]],
+  ['error_8',['error',['../class_j_control_loop.html#a100befc78d818454698a2cccafeb9aa8',1,'JControlLoop']]]
 ];
