e2bfe120f466422d1acdb4486673121cf5e2b772
joshua-8 <joshua262144@gmail.com>
2024-12-04 18:17:15 -0800

    v0.28.3

--- src/JMotorDriver/JMotorDriverPCA9685HBridge.h
+++ src/JMotorDriver/JMotorDriverPCA9685HBridge.h
@@ -19,6 +19,22 @@ protected:
     PCA9685& pca9685;
 
 public:
+    /**
+     * @brief  Controls an H-bridge motor driver controlled by a PCA9685
+     * @note  The PCA9685 must be initialized before this class is used to control a motor
+     * @param  _pca9685: reference to an instance of janelia-arduino/PCA9685
+     * @param  _channelPos: PCA channel that makes the motor go forward
+     * @param  _channelNeg: PCA channel that makes the motor go backward
+     * @param  _reverse: (bool, default=false) set to true if the motor is wired backwards
+     * @param  _breakWhenEnabled: (bool, default=true) set to true to turn on electrical break mode when motor is enabled and speed is 0
+     * @param  _breakWhenDisabled: (bool, default=false) set to true to turn on electrical break mode when motor is disabled
+        example PCA9685 setup:
+        Wire1.begin(PIN_I2C_SDA_IMU, PIN_I2C_SCL_IMU, 400000);
+        pca9685.setupSingleDevice(Wire1, 0x40);
+        pca9685.setupOutputEnablePin(12);
+        pca9685.enableOutputs(12);
+        pca9685.setToFrequency(1500);
+     */
     JMotorDriverPCA9685HBridge(PCA9685& _pca9685, byte _channelPos, byte _channelNeg, bool _reverse = false, bool _breakWhenEnabled = true, bool _breakWhenDisabled = false)
         : pca9685(_pca9685)
     {
@@ -29,10 +45,16 @@ public:
         breakWhenEnabled = _breakWhenEnabled;
         breakWhenDisabled = _breakWhenDisabled;
     }
+    /**
+     * @brief  activate electrical break mode when motor is enabled and speed is 0
+     */
     void setBreakWhenEnabled(bool _breakWhenEnabled)
     {
         breakWhenEnabled = _breakWhenEnabled;
     }
+    /**
+     * @brief  activate electrical break mode when motor is disabled
+     */
     void setBreakWhenDisabled(bool _breakWhenDisabled)
     {
         breakWhenDisabled = _breakWhenDisabled;
